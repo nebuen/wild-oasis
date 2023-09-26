4ab79018e1369f3de901c95e1eae0e3a323f2801
normanebuen <norman.ebuen@gmail.com>
2023-09-26 20:32:00 +0800

    fixed filter function

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  // since its custom hook, we can use other hooks inside it
+  const [searchParams] = useSearchParams();
+
+  //FILTER
+  // all because we want to show `all` bookings by default
+  const filterValue = searchParams.get("status") || "all";
+  const filter =
+    !filterValue || filterValue === "all"
+      ? null
+      : { field: "status", value: filterValue };
+
   const {
     isLoading,
     data: bookings,
     error,
   } = useQuery({
     queryKey: ["bookings"],
-    queryFn: getBookings,
+    // create new function which is the arrow function so we can pass in that object of options
+    queryFn: () => getBookings({ filter }),
   });
 
   return { isLoading, error, bookings };
