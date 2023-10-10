d7599532ed9396fcc8ca7cefac0b79c543895b52
normanebuen <norman.ebuen@gmail.com>
2023-10-10 13:28:23 +0800

    added protected route

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -1,4 +1,12 @@
 function ProtectedRoute({ children }) {
+  // 1. Load the authenticated user
+
+  // 2. While loading, show spinner
+
+  // 3. If user is not authenticated, redirect to login page
+
+  // 4. If user is authenticated, show the children, or render the app
+
   return children;
 }
 
