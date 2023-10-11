815a790c1e7021b0a3d751aef620cf92812d2bbb
normanebuen <norman.ebuen@gmail.com>
2023-10-11 11:41:22 +0800

    logout function

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -9,7 +9,7 @@ export function useLogin() {
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      navigate("/dashboard");
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
       console.log("ERROR", err);
